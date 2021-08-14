c02d1358270235c1f937c65a902064ce62cbc359
André Luís Luza <luza.andre@gmail.com>
2021-08-14 13:37:50 -0300

    adding functions (null model, get legend)

--- R/Functions.R
+++ R/Functions.R
@@ -1,3 +1,27 @@
 
 ## logic function
 `%notin%` <- function(x,y) !(x %in% y)
+
+# the null model to be applied across comms
+sampling_prevalence_NM <- function (pool, local,nsamples){## 
+  sampling <- lapply (as.list(seq(1, nrow (pool))), function (k) ## over all sites
+    lapply (as.list(rep(1,nsamples)), function (i) ## replicate 1 x niter
+      replicate(i,sample (x=pool[k,],size=local[k],prob=pool[k,], replace=F)))) ## do the prevalence sampling
+  # obtain only the sampled probabilities
+  result_list <- vector ("list")
+  result_list$probabilities <- lapply (sampling, function (i) do.call (cbind, i))
+  # obtain the random list of sampled species
+  result_list$species <- lapply (sampling, function (k) do.call (cbind, lapply (k, function (i) dimnames(i)[[2]])))
+  # return results
+  return (result_list)
+}
+
+
+
+# get legend
+get_legend<-function(myggplot){
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
