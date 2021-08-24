144edcb8a6fe2edd67c7be4217c16f79e5d21872
andreluza <luza.andre@gmail.com>
2021-08-23 21:32:50 -0300

    organizing the load of packages

--- R/Packages.R
+++ R/Packages.R
@@ -1,54 +1,33 @@
 # load packages
 
+# maps
 require (raster)
-require(clipr)# clear_clip()
 require(rgdal)
+require(maps)
+require(sp)
+require(rgeos)
+
+# data formatting and transformation
+require(here)
 require(vegan)
 require(reshape2)
 require (reshape)
-require(parallel)
-require(maps)
-require(hypervolume)
+require(clipr)# clear_clip()
 
+# analyses
+require(parallel)
+require(hypervolume)
 # install "probpool" Available here: https://rdrr.io/github/ChrKoenig/probpool/
 #install.packages("remotes") 
 #remotes::install_github("ChrKoenig/probpool")
-
 require(probpool)
 
 # plots
-
 library(rasterVis)
-library(rgdal)
 library(viridis)
 require(ggplot2)
-
 require("rnaturalearth")
 require("rnaturalearthdata")
-
-require(vegan)
-require(here)
-require(raster)
-require(hypervolume)
-require(parallel)
-
-require(reshape)
-require(sp)
-require(here)
-
-
-require(rgdal)
-require(here)
-require(sp)
-require(rgeos)
-require(raster)
-
-
-library(rasterVis)
-library(rgdal)
-library(viridis)
 library("gridExtra")
-
-
 library(ggpubr)
 require(grid)
